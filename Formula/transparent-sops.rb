6e4e3876717ff17e541efe4b6211886417117c95
James Waters <j-waters@users.noreply.github.com>
2025-12-23 00:49:36 +0000

    transparent-sops 0.1.3

--- Formula/transparent-sops.rb
+++ Formula/transparent-sops.rb
@@ -1,8 +1,8 @@
 class TransparentSops < Formula
     desc "Transparent git encryption tool using SOPS"
     homepage "https://github.com/j-waters/transparent-sops"
-    url "https://github.com/j-waters/transparent-sops/archive/refs/tags/v0.1.2.tar.gz"
-    sha256 "3f9731bba57d2174fdd6de28b44f34932c34631273fa050059af7e935f6a2895"
+    url "https://github.com/j-waters/transparent-sops/archive/refs/tags/v0.1.3.tar.gz"
+    sha256 "04cbd4afc511e2b0cf31dada19547f2db9e6630064bff656ab1619fba4bc1672"
     license "MIT"
   
     depends_on "sops"
