2fb6d91f274c3375cdc75c15c1511b2501b3841b
James Waters <james@jcwaters.co.uk>
2026-09-25 04:27:43 +0100

    kitty-linux 0.49.1 (#24)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Casks/kitty-linux.rb
+++ Casks/kitty-linux.rb
@@ -3,9 +3,9 @@
 cask "kitty-linux" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.49.0"
-  sha256 arm64_linux:  "e7a0b9187f160d88a6dd9d836e523a24d7d35502e0afb46645c757c9974d26b7",
-         x86_64_linux: "ed2c015dcdea52bad5cb70461944e487af6c3f79eff4a6542717fcd315feaec2"
+  version "0.49.1"
+  sha256 arm64_linux:  "828fcfe3e165c84d830f82545b6d568655b2620d77b7d5a4360e082bd9ba744b",
+         x86_64_linux: "8cfd68ed484d9a32e4e389abffe1a0ec6e0fbd7be5c9ea1c4fa41b9ead4af791"
 
   url "https://github.com/kovidgoyal/kitty/releases/download/v#{version}/kitty-#{version}-#{arch}.txz"
   name "kitty"
